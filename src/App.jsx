bc2ab175ff6d78c654f40d787cbd60d72ba95510
Clearview Carehome <clearview@Clearviews-iMac.local>
2024-10-07 20:03:29 -0700

    fix homepage and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,34 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:9000";
 
 function App() {
+  /* eslint-disable no-unused-vars */
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  /* eslint-enable no-unused-vars */
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        console.log("error with api fetch-jj");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  console.log(cities);
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +41,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>list of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>countries</p>} />
           <Route path="form" element={<p>form</p>} />
         </Route>
